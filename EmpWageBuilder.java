c09581437b0fd3edda0bdade0e4e26471335bbb1
Sandypandit <sandypandit86@gmail.com>
2021-10-04 18:30:18 +0530

    Committing EmpWageBuilder's branch Attendance

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,7 +1,11 @@
 public class EmpWageBuilder{
-
   public static void main(String[] arg){
 	
-         //Employee wage builder
-     }
+    final int EMP_CHECK = 1; //Declaring constant
+	double empCheck = Math.floor(Math.random() * 10) % 2;
+    System.out.println("the value " + empCheck);
+	//checking attendance using if condition below
+     if(EMP_CHECK == empCheck){ System.out.println("Employee is present"); }
+     else{ System.out.println("Employee is absent"); }
+    }
 }
