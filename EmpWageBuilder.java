1b4b5e6824e21da000384abbb0281990fea0089e
Sandypandit <sandypandit86@gmail.com>
2021-10-04 20:35:49 +0530

    Committing EmpWageBuilder's branch MonthlyWage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,7 +1,14 @@
-public class EmpWageBuilder{
+import java.text.DecimalFormat;
 
-  public static void main(String[] arg){
-	
-         //Employee wage builder
-     }
+public class EmpWageBuilder{
+  public static void main(String[] arg){	
+         //Monthly wage Calculation
+    DecimalFormat df = new DecimalFormat("#,###");
+	double dailyWage = 1000;
+	int workDays = 20;
+
+	double monthlyWage = dailyWage*workDays;
+	System.out.println("Daily wage of Employee is Rs 1000");
+	System.out.println("Monthly wage of employee is Rs " + df.format(monthlyWage));
+    }
 }
