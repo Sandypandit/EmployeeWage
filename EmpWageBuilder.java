d6a80759e070fcf54c986540a0081d2fe85ea753
Sandypandit <sandypandit86@gmail.com>
2021-10-04 20:27:49 +0530

    Committing EmpWageBuilder's branch UseSwitchCase

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,7 +1,31 @@
 public class EmpWageBuilder{
 
-  public static void main(String[] arg){
+  public static void main(String[] arg){	
+         //Using Switch Case
+    Random random = new Random();
+	int empType = random.nextInt(3);
 	
-         //Employee wage builder
-     }
+	int workHour = 8;
+	int wagePerHour = 20;
+	int partTimeWage = 10;
+	
+	switch (empType) 
+      {
+          //comparing value of variable against each case
+        case 0:
+          System.out.println("Fulltime Employee");
+		  int dailyWage1 = workHour*wagePerHour;
+		  System.out.println("Daily wage of Fulltime employee is " + dailyWage1 +"Rs");
+          break;
+        case 1:
+          System.out.println("Part time Employee");
+		  int dailyWage = workHour*partTimeWage;
+		  System.out.println("Daily wage of Fulltime employee is " + dailyWage +"Rs");
+          break;
+        case 2:
+          System.out.println("Employee is absent");
+          break;
+
+    }
+  }
 }
