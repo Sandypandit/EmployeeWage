a8d74c3404f866f8c334c3dcf36a9c7707837034
Sandypandit <sandypandit86@gmail.com>
2021-10-04 20:10:38 +0530

    Committing EmpWageBuilder's branch DailyWage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,7 +1,10 @@
 public class EmpWageBuilder{
 
   public static void main(String[] arg){
-	
-         //Employee wage builder
+	//Employee wage builder
+	int workHour = 8;
+	int wagePerHour = 20;
+	int dailyWage = workHour*wagePerHour;  //Calculation of daily wage
+	System.out.println("Daily wage of employee is " + dailyWage +"Rs");
      }
 }
