c0fa2c3830b3dd5444f9648cd7a062574707ccce
Sandypandit <sandypandit86@gmail.com>
2021-10-04 20:45:01 +0530

    Committing EmpWageBuilder's branch WageConditions

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,7 +1,23 @@
-public class EmpWageBuilder{
+import java.util.Random;
+import java.text.DecimalFormat;
 
-  public static void main(String[] arg){
-	
-         //Employee wage builder
-     }
+public class EmpWageBuilder{
+  public static void main(String[] arg){	
+         //Complete conditions to get wage
+	DecimalFormat df = new DecimalFormat("#,###");
+    
+	Random random = new Random();
+	int workHoursComp = random.nextInt(98,101); //getting working HOURS
+    int workDaysComp = random.nextInt(18,21); //getting working DAYS
+
+	double dailyWage = 1000;
+	int workDays = 20;
+	int totalWorkHours = 100;
+	double monthlyWage = dailyWage*workDays;
+	While(workHoursComp <=100 && workDaysComp == 20){
+	System.out.println("Good, you have completed montly working hours and days");
+	System.out.println("Monthly wage of employee is Rs " + df.format(monthlyWage));
+    }
+	else{ System.out.println("You haven't completed working hours of 100 hours or working days of 20 days"); };
+  }
 }
